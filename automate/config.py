a6a7c9167adb2c7fb3d3045105c4aa844e7417d8
Richy8 <menaelvisjones@gmail.com>
2019-06-22 08:22:10 +0100

    updated

--- automate/config.py
+++ automate/config.py
@@ -1,6 +1,6 @@
 # Configuration settings
 class Config:
-    SECRET_KEY = '20352be0164409e672afb55de0e9fefe'
+    SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:april2019@localhost/countymgtdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SINGLE_DATA_PER_PAGE = 11
@@ -8,5 +8,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'menaelvisjones@gmail.com'
-    MAIL_PASSWORD = 'violatemena'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
